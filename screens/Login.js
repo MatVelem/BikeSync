f09218d3d9a5130fde12981bf90c9c223f773dc7
MatVelem <95192372+MatVelem@users.noreply.github.com>
2024-10-19 15:50:01 -0300

    Validação de dados na tela de login
    Tela de login estava passando para a próxima mesmo sem ter dados inseridos no campo, agora é necessário inserir dados para o login ser efetuado

--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image, Alert } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
 import { useState } from 'react';
 
 export default function Login({navigation}) {
+  
+  const [email, setEmail] = useState(''); // Adiciona estado para o email
+  const [senha, setSenha] = useState(''); // Adiciona estado para a senha
+  const [showPassword, setShowPassword] = useState(false);
 
   const entrar = () => {
+    // Verifica se os campos de email e senha estão vazios
+    if (email === '' || senha === '') {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+      return;
+    }
+
+    // Se passar na validação, navega para a tela Principal
     navigation.reset({
       index: 0,
       routes: [{name: "Principal"}]
     });
   };
-
-  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -36,6 +45,8 @@ export default function Login({navigation}) {
           leftIcon={{ type: 'font-awesome', name: 'user' }}
           containerStyle={styles.input}
           keyboardType="email-address"
+          value={email} // Vincula o estado do e-mail
+          onChangeText={setEmail} // Atualiza o estado ao digitar
         />
 
         <Input
@@ -43,6 +54,8 @@ export default function Login({navigation}) {
           leftIcon={{ type: 'font-awesome', name: 'lock' }}
           containerStyle={styles.input}
           secureTextEntry={!showPassword}
+          value={senha} // Vincula o estado da senha
+          onChangeText={setSenha} // Atualiza o estado ao digitar
           rightIcon={
             <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
               <Icon
@@ -91,7 +104,6 @@ const styles = StyleSheet.create({
     marginBottom: 20, 
   },
   title: {
-
     fontSize: 32,
     fontWeight: 'bold',
     color: '#000000',
